4b363730500d2262d7a17f1a85961743fa46ad50
Bernd <Bernd@6aeb069d-d5e0-465d-890a-ccf59a9091f3>
2014-11-25 18:00:11 +0000

    linux main
    git-svn-id: svn+ssh://blue.3dallee.de/fhtw/trunk@10 6aeb069d-d5e0-465d-890a-ccf59a9091f3

---
+++ AirGapC/main_linux.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+#define CONNECT(source ,target) source ## _ ## ReportData = &target ## _ ## OnData
+
+#include "AudioSource.h"
+#include "BandPassFilter.h"
+#include "SimpleToComplex.h"
+#include "Multiply.h"
+#include "QuadraturDemodulator.h"
+#include "ClockRecovery.h"
+#include "BinarySlicer.h"
+#include "FileSink.h"
+#include "FileSource.h"
+#include "BitToSymbol.h"
+#include "FirFilter.h"
+#include "FrequencyModulator.h"
+#include "ComplexToSimple.h"
+#include "AudioSink.h"
+
+int main(int argc, char **argv)
+{
+	bool isReceiver = true;
+
+	if (isReceiver){
+		CONNECT(AudioSource, BandPassFilter);
+		CONNECT(BandPassFilter, SimpleToComplex);
+		CONNECT(SimpleToComplex, Multiply);
+		CONNECT(Multiply, QuadraturDemodulator);
+		CONNECT(QuadraturDemodulator, ClockRecovery);
+		CONNECT(ClockRecovery, BinarySlicer);
+		CONNECT(BinarySlicer, FileSink);
+
+		AudioSource_Work();
+	}
+	else
+	{
+		CONNECT(FileSource, BitToSymbol);
+		CONNECT(BitToSymbol, FirFilter);
+		CONNECT(FirFilter, FrequencyModulator);
+		CONNECT(FrequencyModulator, Multiply);
+		CONNECT(Multiply, ComplexToSimple);
+		CONNECT(ComplexToSimple, AudioSink);
+
+		FileSource_Work();
+	}
+
+
+	return 0;
+}
