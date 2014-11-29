e91b156bf97473893cc9883a2cb9fa95df28fa04
Bernd <Bernd@6aeb069d-d5e0-465d-890a-ccf59a9091f3>
2014-11-29 21:41:02 +0000

    audio sink
    git-svn-id: svn+ssh://blue.3dallee.de/fhtw/trunk@24 6aeb069d-d5e0-465d-890a-ccf59a9091f3

--- AirGapC/AudioSink_DirectSound.c
+++ AirGapC/AudioSink_DirectSound.c
@@ -7,14 +7,57 @@
 #include <math.h>
 
 #define BUFFER_SIZE (1024*128)
+#define numbuffers 8
 
 extern void AudioSink_OnData(ComplexPackage);
 
+void Init();
+LPDIRECTSOUNDBUFFER soundBuffer = 0;
+
+
 void AudioSink_OnData(ComplexPackage data)
+{
+	if (soundBuffer == 0)
+		Init();
+
+	//do something with data
+
+	void *buffer = 0;
+	DWORD bufferLen = 0;
+	LPVOID buffer2 = 0;
+	DWORD bufferLen2 = 0;
+
+	DWORD p1, p2;
+
+	HRESULT result = soundBuffer->lpVtbl->GetCurrentPosition(soundBuffer, &p1, &p2);
+
+	//TODO: wait if buffer is full
+
+	result = soundBuffer->lpVtbl->Lock(soundBuffer, 0, BUFFER_SIZE, &buffer, &bufferLen, &buffer2, &bufferLen2, 0);
+
+	short *samples = (short *)buffer;
+
+	for (int i = 0; i < BUFFER_SIZE / 2; i++)
+	{
+		float x = (float)i;
+		x *= 2 * 3.1415f;
+		x /= 22;
+		float y = sinf(x);
+
+		samples[i] = 0;
+	}
+
+	result = soundBuffer->lpVtbl->Unlock(soundBuffer, buffer, bufferLen, buffer2, bufferLen2);
+
+
+
+}
+
+void Init()
 {
 	LPDIRECTSOUND directSound;
 	HRESULT result = DirectSoundCreate(NULL, &directSound, NULL);
-	
+
 	result = directSound->lpVtbl->SetCooperativeLevel(directSound, GetDesktopWindow(), DSSCL_EXCLUSIVE);
 
 	WAVEFORMATEX waveformat;
@@ -36,16 +79,33 @@ void AudioSink_OnData(ComplexPackage data)
 	bufferDescription.dwBufferBytes = BUFFER_SIZE;
 	bufferDescription.lpwfxFormat = &waveformat;
 
-	LPDIRECTSOUNDBUFFER soundBuffer;
 	result = directSound->lpVtbl->CreateSoundBuffer(directSound, &bufferDescription, &soundBuffer, NULL);
 
-	LPVOID buffer=0;
-	DWORD bufferLen=0;
-	LPVOID buffer2=0;
-	DWORD bufferLen2=0;
+	LPDIRECTSOUNDNOTIFY notify;
+
+	const IID *iid = &IID_IDirectSoundNotify;
+
+	result = soundBuffer->lpVtbl->QueryInterface(soundBuffer, iid, (VOID**)&notify);
+
+	HANDLE event = CreateEvent(NULL, FALSE, FALSE, NULL);
+
+
+	DSBPOSITIONNOTIFY positionNotify[numbuffers];
+
+	for (int i = 0; i < numbuffers; i++)
+	{
+		positionNotify[i].dwOffset = (BUFFER_SIZE / numbuffers * (i + 1)) - 1;
+		positionNotify[i].hEventNotify = event;
+	}
+
+
+	LPVOID buffer = 0;
+	DWORD bufferLen = 0;
+	LPVOID buffer2 = 0;
+	DWORD bufferLen2 = 0;
 
 	result = soundBuffer->lpVtbl->Lock(soundBuffer, 0, BUFFER_SIZE, &buffer, &bufferLen, &buffer2, &bufferLen2, 0);
-	
+
 	short *samples = (short *)buffer;
 
 	for (int i = 0; i < BUFFER_SIZE / 2; i++)
@@ -55,16 +115,12 @@ void AudioSink_OnData(ComplexPackage data)
 		x /= 22;
 		float y = sinf(x);
 
-		samples[i] = (short) (y * 30000);
+		samples[i] = 0;
 	}
-	
+
 	result = soundBuffer->lpVtbl->Unlock(soundBuffer, buffer, bufferLen, buffer2, bufferLen2);
 
+	result = notify->lpVtbl->SetNotificationPositions(notify, numbuffers, positionNotify);
 
 	result = soundBuffer->lpVtbl->Play(soundBuffer, 0, 0, DSBPLAY_LOOPING);
-
-	Sleep(15000);
-
-
-	//do something with data
 }
