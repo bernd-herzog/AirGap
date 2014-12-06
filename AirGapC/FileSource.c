507175bdfb599af1f23be31183b0ff7363ce7529
Markus <Markus@6aeb069d-d5e0-465d-890a-ccf59a9091f3>
2014-12-06 17:17:11 +0000

    added a simple testdata.out which should include the text that's been read char by char by FileSource.c which in turn transforms it into an array ob bits(bool)
    git-svn-id: svn+ssh://blue.3dallee.de/fhtw/trunk@37 6aeb069d-d5e0-465d-890a-ccf59a9091f3

--- AirGapC/FileSource.c
+++ AirGapC/FileSource.c
@@ -2,6 +2,7 @@
 #include "DataTypes.h"
 #include <stdbool.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 extern void(*FileSource_ReportData)(BoolPackage);
 extern void FileSource_Work();
@@ -10,14 +11,47 @@ void FileSource_Work()
 {
 	BoolPackage ret;
 	const char *filename = "testdata.out";
+	FILE *i_file;
+	i_file = fopen(filename, "rb");
+	fseek(i_file, 0, SEEK_END);
+	long fsize = ftell(i_file);
+	fseek(i_file, 0, SEEK_SET);
+
+	char *text = malloc(fsize +1);
+	fread(text, fsize, 1, i_file);
+	fclose(i_file);
+
+	text[fsize] = 0;
 
 	//TODO: datei öffnen
 	//TODO: länge auf anzahl der bits in der datei setzen
-	ret.count = 0;
+	ret.count = 8 * fsize;
 
 	ret.data = (bool *)malloc(ret.count * sizeof(bool));
 
 	//TODO: inhalt der datei in <ret.data> bit für bit ablegen
+	for (int i = 0; i < fsize ; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			if (((text[i] >> j) & 0x00000001) == true){
+				ret.data[i * 8 + (7-j)] = true;
+			}else{
+				ret.data[i * 8 + (7-j)] = false;
+			}
+		}
+	}
+
+	/* To check if first char is correct:
+	ret.data[0];
+	ret.data[1];
+	ret.data[2];
+	ret.data[3];
+	ret.data[4];
+	ret.data[5];
+	ret.data[6];
+	ret.data[7];*/
+
 
 	FileSource_ReportData(ret);
 	free(ret.data);
