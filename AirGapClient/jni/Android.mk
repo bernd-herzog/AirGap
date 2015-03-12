86115458d3f1d476d8d9e9315e5328bd0960c7e9
Bernd <Bernd@6aeb069d-d5e0-465d-890a-ccf59a9091f3>
2015-03-12 19:39:57 +0000

    src einarbeitung
    git-svn-id: svn+ssh://blue.3dallee.de/fhtw/trunk@75 6aeb069d-d5e0-465d-890a-ccf59a9091f3

--- AirGapClient/jni/Android.mk
+++ AirGapClient/jni/Android.mk
@@ -6,6 +6,6 @@ LOCAL_CFLAGS += -std=c99
 
 
 LOCAL_MODULE    := AirGap
-LOCAL_SRC_FILES := AirGapMain.cpp ../jni-src/Depacketizer.c ../jni-src/MemorySink.c BinarySlicer.c ClockRecovery.c QuadraturDemodulator.c Multiply.c FirFilter.c agmath.c ReedSolomon.c
+LOCAL_SRC_FILES := src/AirGapMain.cpp src/Depacketizer.c src/MemorySink.c src/BinarySlicer.c src/ClockRecovery.c src/QuadraturDemodulator.c src/Multiply.c src/FirFilter.c src/agmath.c src/ReedSolomon.c
 
 include $(BUILD_SHARED_LIBRARY)
