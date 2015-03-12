bde5e8c36589b4790fcd8bf493eaf7af2f2534f5
Bernd <Bernd@6aeb069d-d5e0-465d-890a-ccf59a9091f3>
2015-03-12 19:17:19 +0000

    git-svn-id: svn+ssh://blue.3dallee.de/fhtw/trunk@74 6aeb069d-d5e0-465d-890a-ccf59a9091f3

--- AirGapClient/jni/Android.mk
+++ AirGapClient/jni/Android.mk
@@ -6,6 +6,6 @@ LOCAL_CFLAGS += -std=c99
 
 
 LOCAL_MODULE    := AirGap
-LOCAL_SRC_FILES := AirGapMain.cpp Depacketizer.c MemorySink.c BinarySlicer.c ClockRecovery.c QuadraturDemodulator.c Multiply.c FirFilter.c agmath.c ReedSolomon.c
+LOCAL_SRC_FILES := AirGapMain.cpp ../jni-src/Depacketizer.c ../jni-src/MemorySink.c BinarySlicer.c ClockRecovery.c QuadraturDemodulator.c Multiply.c FirFilter.c agmath.c ReedSolomon.c
 
 include $(BUILD_SHARED_LIBRARY)
--- AirGapClient/jni/MemorySink.h
+++
@@ -1,9 +0,0 @@
-#include "DataTypes.h"
-
-void MemorySink_OnData(UCharPackage);
-char *MemorySink_GetMemory();
-void MemorySink_Clear();
-
-
-extern int MemorySink_len;
-extern char *MemorySink_data;
